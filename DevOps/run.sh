46956adafda9e8bb3ffc382c8839c5aa787c4eb6
Ian-Lo <ian.chi.yan.lo@gmail.com>
2020-10-20 18:24:46 +1100

    Bash scripting (#16)
    * Creating bash script for running on NLP cluster
    
    * script
    
    * bash shell updated and waiting on parameter modification to Test-Train.py
    
    Co-authored-by: Ian Lo <ilo@uni.sydney.edu.au>

---
+++ DevOps/run.sh
@@ -0,0 +1,36 @@
+# Bash file for NLP execution
+
+# Download GIT
+echo 'Clone github.com/Ian-Lo/DATA5703_Group09'
+git init
+git config user.email "email"
+git config user.name "user"
+git pull https://3b93b47605174bb4a2305d0d1bc6f5a72d708130:x-oauth-basic@github.com/Ian-Lo/DATA5703_Group09 main
+
+pip install -r 'DATA5703_Group09/DevOps/requirements.txt'
+
+# Download Dataset
+echo 'Download GDriveDL and make executable'
+curl https://raw.githubusercontent.com/matthuisman/gdrivedl/master/gdrivedl.py --output GDriveDL
+chmod +x GDriveDL
+
+echo 'Download datasets'
+./GDriveDL 'https://googledrive.com/host/a6IU&id=1QmcBWivFeArJCVRwjKe6B6nZrTlom47W'
+
+./GDriveDL 'https://drive.google.com/file/d/1mXjh_GBtaPz0B_asBzaWkyKrbHTKrn0t/view?usp=sharing'
+
+./GDriveDL 'https://drive.google.com/file/d/1xJ-1mDK6DimqKH76LcKHFVPolqUOLOCD/view?usp=sharing'
+
+
+# Untar files downloaded from google drive
+echo 'Untaring files'
+tar xvf Dataset_train_100k.tar.gz
+tar xvf Dataset_test.tar.gz
+tar xvf Dataset_dev.tar.gz
+
+# Call training
+# Pass path and n iters as command line parameters to training command
+# Paths needs to be relative
+Test-Train.py arg1 arg2 arg3
+
+echo 'DID YOU REMEMBER TO UNCOMMENT THE LINES YOU NEED TO EXECUTE???'

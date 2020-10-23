d8bb422a5836aa6716cf1f14d9caab28f341d41f
Ian Lo <ilo@uni.sydney.edu.au>
2020-10-23 15:19:50 +1100

    made sure pip was running as python3

--- DevOps/run.sh
+++ DevOps/run.sh
@@ -7,7 +7,7 @@ git config user.email "email"
 git config user.name "user"
 git pull https://3b93b47605174bb4a2305d0d1bc6f5a72d708130:x-oauth-basic@github.com/Ian-Lo/DATA5703_Group09 main
 
-pip install -r 'DevOps/requirements.txt'
+python3 -m pip install -r 'DevOps/requirements.txt'
 
 # Download Dataset
 echo 'Download GDriveDL and make executable'

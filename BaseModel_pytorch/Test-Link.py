2b2e10516a3d9ca7cb90c8772d22c8e17f7524f6
andersborges <andersborges@hotmail.com>
2020-12-02 13:35:05 +1100

    before forking to test link

--- BaseModel_pytorch/Test-Link.py
+++ BaseModel_pytorch/Test-Link.py
@@ -57,41 +57,36 @@ alpha_c_cell_content = 0.0
 structural_encoder_conv = False
 cell_content_encoder_conv = False
 
-convs = [False, True]
-outsize = [64, 256]
-
 from Model import Model
 from matplotlib import pylab as plt
 
-for conv in convs:
-    for out in outsize:
-        # instantiate model
-        model = Model(relative_path,
-                        model_tag,
-                        in_channels = in_channels,
-                        out_channels_structural = out,
-                        out_channels_cell_content = out_channels_cell_content,
-                        structural_embedding_size=structural_embedding_size,
-                        structural_hidden_size=structural_hidden_size,
-                        structural_attention_size=structural_attention_size,
-                        cell_content_embedding_size=cell_content_embedding_size,
-                        cell_content_hidden_size=cell_content_hidden_size,
-                        cell_content_attention_size=cell_content_attention_size,
-                        structural_encoder_conv = conv)
+# instantiate model
+model = Model(relative_path,
+                model_tag,
+                in_channels = in_channels,
+                out_channels_structural = out_channels_structural,
+                out_channels_cell_content = out_channels_cell_content,
+                structural_embedding_size=structural_embedding_size,
+                structural_hidden_size=structural_hidden_size,
+                structural_attention_size=structural_attention_size,
+                cell_content_embedding_size=cell_content_embedding_size,
+                cell_content_hidden_size=cell_content_hidden_size,
+                cell_content_attention_size=cell_content_attention_size,
+                structural_encoder_conv = False)
 
-        loss,loss_s, loss_cc, loss_val, loss_s_val, loss_cc_val = model.train(epochs=epochs,
-                    lambdas=lambdas,
-                    lrs=lrs,
-                    number_examples=number_examples,
-                    number_examples_val=number_examples_val,
-                    batch_size=batch_size,
-                    batch_size_val = batch_size_val,
-                    storage_size=storage_size,
-                    val = val,
-                    maxT_val = maxT_val,
-                    alpha_c_struc = alpha_c_struc,
-                    alpha_c_cell_content = alpha_c_cell_content)
-        plt.plot(loss, label = 'Conv:%s, out_size:%d'%(conv, out))
+loss,loss_s, loss_cc, loss_val, loss_s_val, loss_cc_val = model.train(epochs=epochs,
+            lambdas=lambdas,
+            lrs=lrs,
+            number_examples=number_examples,
+            number_examples_val=number_examples_val,
+            batch_size=batch_size,
+            batch_size_val = batch_size_val,
+            storage_size=storage_size,
+            val = val,
+            maxT_val = maxT_val,
+            alpha_c_struc = alpha_c_struc,
+            alpha_c_cell_content = alpha_c_cell_content)
+plt.plot(loss, label = 'Conv:%s, out_size:%d'%(conv, out))
 
 plt.title("Training with different encoders and with different output dimensions")
 plt.ylabel("Loss")
